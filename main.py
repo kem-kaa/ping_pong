bb3325b14b449b6009635dcfb97b3d4bac853c3e
kem-kaa <kamillapandina@gmail.com>
2026-05-28 12:17:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+from settings import *
+
+import ball
+
+pygame.init()
+
+pygame.display.set_caption('пинг-понг')
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+                                 
+running = True
+
+while running:
+    #отслеживание действий
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    #апдейты
+
+
+    #отрисовка
+    screen.fill((99, 8, 54))
+
+    ball.draw_ball(screen)
+
+    pygame.display.update()
+    clock.tick(FPS)
